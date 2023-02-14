98c43af1838e707d1585b6bba2e6846dc58af931
Kirill Kantur <ludivopasnosti@gmail.com>
2023-02-14 18:42:57 +0300

    Fina HW Sem1

---
+++ gr9363/sem1Task4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача №4
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+Console.Write("Введите число A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число C: ");
+int numC = Convert.ToInt32(Console.ReadLine());
+if (numA>numB)
+{
+    if (numA>numC)
+    {
+        Console.WriteLine(numA);
+    }
+    else
+    {
+        Console.WriteLine(numC);
+    }
+}    
+else
+{
+    if (numB>numC)
+    {
+        Console.WriteLine(numB);
+    }
+    else
+    {
+    Console.WriteLine(numC);
+    }
+}    
--- gr9363/sem1task2/Program.cs
+++ gr9363/sem1task2/Program.cs
@@ -1,16 +1,15 @@
-﻿//Задача №3//Напишите программу, которая будет выдавать название дня недели по заданному номеру.
-Console.Write("Введите номер дня недели: ");
-string? lineDay = Console.ReadLine();
-if(lineDay!=null)
-object value = {
+﻿// Задача №2
+// Напишите программу, которая на входе принимает два числа и выдает, какое число большее, а какое меньшее
 
-    int day = int.Parse(lineDay);
-    string[] dayWeek = new string[7];
-    dayWeek[0] = "Понедельник";   
-    dayWeek[1] = "Вторник";   
-    dayWeek[2] = "Среда"; 
-    dayWeek[3] = "Четверг";  
-    dayWeek[4] = "Пятница";  
-    dayWeek[5] = "Суббота";  
-    dayWeek[6] = "Воскресенье"
-Console.WriteLine(dayWeek[day-1]);}
+Console.Write("Введите число A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+if (numA>numB)
+{
+    Console.WriteLine("Число А больше числа Б");
+}
+else
+{
+    Console.WriteLine("Число Б больше числа А");
+}
