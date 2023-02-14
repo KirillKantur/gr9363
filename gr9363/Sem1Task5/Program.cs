10381742f4107b4eb25110fcdf3947ffd5b615c0
Kirill Kantur <ludivopasnosti@gmail.com>
2023-02-14 16:48:20 +0300

    sem1 Task5

---
+++ gr9363/Sem1Task5/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу которая на входе примит число (n), а на выходе показывает все целые числа в промежутке от -n до n
+
+// Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустыми
+if(inputLine != null)
+{
+    // Парсим введеное число
+    int  inputNumber = int.Parse(inputLine);
+
+    int startNumber = inputNumber*(-1);
+
+    //Выходное значение
+    string outLine = string.Empty;
+
+    while (startNumber<inputNumber)
+    {
+        outLine = outLine + startNumber + ", ";
+        //startNumber=startNumber+1
+        startNumber++;
+        
+    }
+    outLine = outLine + inputNumber;
+
+    // Выводим данные в консоле
+    Console.WriteLine(outLine);
+}
